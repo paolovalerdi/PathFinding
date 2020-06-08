a8d8ebf2f05468f35ef52865a33185c32153ec56
Paolo Valerdi <paolovalerdi@gmail.com>
2020-06-08 11:43:58 -0500

    Add some extra funcitions to our node

--- node.cpp
+++ node.cpp
@@ -1,4 +1,5 @@
 #include "node.h"
+#include <QtDebug>
 
 Node::Node()
 {
@@ -8,6 +9,8 @@ Node::Node()
     size = 0;
     is_obstacle = false;
     is_visited = false;
+    is_start_node = false;
+    is_end_node = false;
     parent = nullptr;
 }
 
@@ -19,6 +22,8 @@ Node::Node(int x, int y, int padding, int size)
     this->size = size;
     is_obstacle = false;
     is_visited = false;
+    is_start_node = false;
+    is_end_node = false;
     parent = nullptr;
 }
 
@@ -46,6 +51,64 @@ std::vector<Node *> Node::get_neighbours_list()
     return neighbours;
 }
 
+void Node::mark_as_start(bool marked)
+{
+    is_end_node = false;
+    is_obstacle = false;
+    is_visited = false;
+    is_start_node = marked;
+    update();
+}
+
+void Node::mark_as_end(bool marked)
+{
+    is_start_node = false;
+    is_obstacle = false;
+    is_visited = false;
+    is_end_node = marked;
+    update();
+}
+
+void Node::restart()
+{
+    global_goal = INFINITY;
+    local_goal = INFINITY;
+    set_visited(false);
+    parent = nullptr;
+}
+
+void Node::set_local_goal(float value)
+{
+    local_goal = value;
+}
+
+float Node::get_local_goal()
+{
+    return local_goal;
+}
+
+bool Node::get_is_visited()
+{
+    return is_visited;
+}
+
+float Node::get_global_goal()
+{
+    return global_goal;
+}
+
+
+void Node::set_global_goal(float value)
+{
+    global_goal = value;
+}
+
+void Node::set_visited(bool value)
+{
+    is_visited = value;
+    update();
+}
+
 QRectF Node::boundingRect() const
 {
     return QRectF(x + padding, y + padding, size - 10, size - 10);
@@ -53,7 +116,49 @@ QRectF Node::boundingRect() const
 
 void Node::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
+    if(is_obstacle)
+    {
+        painter->setPen(QPen(QColor(160, 161, 164)));
+        painter->setBrush(QBrush(QColor(160, 161, 164)));
+        painter->drawRect(boundingRect());
+        return;
+    }
+    if(is_start_node)
+    {
+
+        painter->setPen(QPen(QColor(105, 240, 174)));
+        painter->setBrush(QBrush(QColor(105, 240, 174)));
+        painter->drawRect(boundingRect());
+        return;
+    }
+    if (is_end_node)
+    {
+        painter->setPen(QPen(QColor(255, 82, 82)));
+        painter->setBrush(QBrush(QColor(255, 82, 82)));
+        painter->drawRect(boundingRect());
+        return;
+    }
+    if(is_path)
+    {
+        painter->setPen(QPen(QColor(255, 196, 0)));
+        painter->setBrush(QBrush(QColor(255, 196, 0)));
+        painter->drawRect(boundingRect());
+        return;
+    }
+    if (is_visited)
+    {
+        painter->setPen(QPen(QColor(90, 91, 104)));
+        painter->setBrush(QBrush(QColor(90, 91, 104)));
+        painter->drawRect(boundingRect());
+        return;
+    }
     painter->setPen(QPen(QColor(70, 71, 84)));
     painter->setBrush(QBrush(QColor(70, 71, 84)));
     painter->drawRect(boundingRect());
+}
+
+void Node::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event)
+{
+    is_obstacle = !is_obstacle;
+    update();
 }
--- node.h
+++ node.h
@@ -9,8 +9,11 @@
 class Node : public QGraphicsItem
 {
 private:
+    bool is_start_node;
+    bool is_end_node;
     bool is_obstacle;
     bool is_visited;
+
     float global_goal;
     float local_goal;
 
@@ -23,18 +26,40 @@ private:
     std::vector<Node*> neighbours;
     Node *parent;
 public:
-
    Node();
    Node(int x, int y, int padding, int size);
    ~Node();
-
+   bool is_path = false;
+   void update_is_path() {
+       is_path = true;
+       update();
+   }
    int get_x();
    int get_y();
    void add_neighbour(Node *neighbour);
    std::vector<Node*> get_neighbours_list();
+   void mark_as_start(bool marked);
+   void mark_as_end(bool marked);
+   void restart();
+   void set_local_goal(float value);
+   float get_local_goal();
+   bool get_is_visited();
+   void set_global_goal(float value);
+   float get_global_goal();
+   void set_visited(bool value);
 
+   bool get_is_obstacle() {
+       return is_obstacle;
+   }
+   void set_parent(Node *value) {
+       parent = value;
+   }
+
+   Node *get_parent(){return parent;}
    QRectF boundingRect() const override;
    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = Q_NULLPTR) override;
+protected:
+   void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
 };
 
 #endif // NODE_H
