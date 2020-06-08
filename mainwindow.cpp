7ebd028f89ab4c27d444f58e253ea216c6abc9b0
Paolo Valerdi <paolovalerdi@gmail.com>
2020-06-08 11:44:15 -0500

    Update UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,21 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#define SQUARE_HEIGHT 40
+#define SQUARE_HEIGHT 30
 #define MAP_SIZE 600
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->setFixedSize(600, 600);
-    this->setCentralWidget(ui->graphicsView);
+   // this->setCentralWidget(ui->graphicsView);
 
-    path_finder = new PathFinder(MAP_SIZE, SQUARE_HEIGHT, 5);
+    path_finder = new PathFinder(this, MAP_SIZE, SQUARE_HEIGHT, 5);
+    this->installEventFilter(path_finder);
+
     ui->graphicsView->setScene(path_finder->get_scene());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    path_finder->solve();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,6 +18,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
     PathFinder *path_finder;
